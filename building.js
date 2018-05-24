98978a00b6cf15cc022e9815e5c40a6c1ffdf019
Dustin Parker <dustin.the.park@gmail.com>
2018-05-24 08:04:31 -0600

    Find next available elevator when building requests one

--- building.js
+++ building.js
@@ -7,15 +7,22 @@ class Building {
     this.elevators = [];
 
     for (let i = 0; i < numberElevators; i++) {
-      this.elevators.push(new Elevator(numberFloors));
+      this.elevators.push(new Elevator(numberFloors, i.toString()));
     }
 
     this.requestElevator = this.requestElevator.bind(this);
   }
 
-  requestElevator() {
+  requestElevator(currentFloor, numberOfPeople = 1) {
+    let availableElevator;
     // an unoccupied elevator is already stopped at that floor
+    availableElevator = this.elevators.find(elevator => elevator.floor === currentFloor);
+    if (availableElevator) {
+      availableElevator.enterElevator(numberOfPeople);
+      return availableElevator;
+    }
     // an occupied elevator is moving and will pass that floor on its way
+    const occupiedElevators = this.elevators.filter(elevator => elevator.numberOccupants > 0);
     // the unoccupied elevator closest to it will answer the call
   }
 }
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const Building = require('./building');
 const Elevator = require('./elevator');
 
-// const twoStoryMansion = new Building(1, 2);
+const twoStoryMansion = new Building(2, 2);
+twoStoryMansion.requestElevator(2);
 
 // console.log(twoStoryMansion);
 
-const myElevator = new Elevator(2);
-myElevator.goToFloor(2);
-myElevator.goToFloor(1);
-myElevator.goToFloor(1);
-myElevator.goToFloor(5);
-myElevator.goToFloor(-1);
+// const myElevator = new Elevator(2);
+// myElevator.goToFloor(2);
+// myElevator.goToFloor(1);
+// myElevator.goToFloor(1);
+// myElevator.goToFloor(5);
+// myElevator.goToFloor(-1);
