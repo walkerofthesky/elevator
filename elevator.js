cf5a2d26305879ea0c6da1a5ea5aabff7e5b1ac0
Dustin Parker <dustin.the.park@gmail.com>
2018-05-24 07:49:38 -0600

    Add serviceElevator, elevator maintenance, track floors passed
    * elevator is in maintenance mode for every 100 trips
    * can't leave maintenance until serviced

--- elevator.js
+++ elevator.js
@@ -6,11 +6,13 @@ class Elevator {
     this.maxFloor = maxFloor;
     this.numberOccupants = 0;
     this.numberTrips = 0;
+    this.floorsPassed = 0;
     this.isInMaintenance = false;
 
     this.openDoor = this.openDoor.bind(this);
     this.enterElevator = this.enterElevator.bind(this);
     this.goToFloor = this.goToFloor.bind(this);
+    this.serviceElevator = this.serviceElevator.bind(this);
   }
 
   openDoor(open = true) {
@@ -26,11 +28,14 @@ class Elevator {
   }
 
   goToFloor(floor) {
-    // update this.floor this.numberTrips
+    if (this.isInMaintenance) {
+      return console.log(`Elevator ${ this.name } is in maintenance`);
+    }
+
     if (floor === this.floor) {
       return console.log(`Elevator ${ this.name } is already on floor ${ floor }.`);
     }
-    
+
     if (floor > this.maxFloor) {
       floor = this.maxFloor;
     } else if (floor < 1) {
@@ -39,9 +44,15 @@ class Elevator {
 
     this.openDoor(false);
 
+    this.floorsPassed += Math.max(this.floor, floor) - Math.min(this.floor, floor);
+
     this.floor = floor;
     this.numberTrips++;
-    console.log(`Elevator ${ this.name } is on floor ${ this.floor }. It has taken ${ this.numberTrips } trip${ this.numberTrips > 1 ? 's' : '' }.`);
+    console.log(`Elevator ${ this.name } is on floor ${ this.floor }. It has taken ${ this.numberTrips } trip${ this.numberTrips > 1 ? 's' : '' }, and has passed ${ this.floorsPassed } floor${ this.numberTrips > 1 ? 's' : '' }.`);
+
+    if (this.numberTrips % 100 === 0) {
+      this.isInMaintenance = true;
+    } 
   }
 
   exitElevator(numberOfPeople) {
@@ -49,6 +60,10 @@ class Elevator {
       this.openDoor();
     }
     this.numberOccupants += numberOfPeople;
+  }
+
+  serviceElevator() {
+    this.isInMaintenance = false;
   }
 }
 
