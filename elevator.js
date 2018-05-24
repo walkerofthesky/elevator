dde4385e04f32dceeba83b4aaa62077a9e3ebaa0
Dustin Parker <dustin.the.park@gmail.com>
2018-05-24 07:20:58 -0600

    Add Elevator methods
    * reportDoorStatus to tell if door is open or closed
    * enterElevator to add occupants
    * goToFloor tracks current floor and number of trips
    * added name to elevator for easier identification

--- elevator.js
+++ elevator.js
@@ -1,5 +1,6 @@
 class Elevator {
-  constructor(maxFloor = 2) {
+  constructor(maxFloor = 2, name = 'solo') {
+    this.name = name;
     this.floor = 1;
     this.doorStatus = 'closed';
     this.maxFloor = maxFloor;
@@ -7,11 +8,29 @@ class Elevator {
     this.numberTrips = 0;
     this.isInMaintenance = false;
 
-    this.requestFloor = this.requestFloor.bind(this);
+    this.reportDoorStatus = this.reportDoorStatus.bind(this);
+    this.enterElevator = this.enterElevator.bind(this);
+    this.goToFloor = this.goToFloor.bind(this);
   }
 
-  requestFloor(floor) {
+  reportDoorStatus() {
+    console.log(`Elevator ${ this.name }'s door is ${ this.doorStatus }` );
+  }
+
+  enterElevator(numberOfPeople) {
+    if (this.doorStatus === 'closed') {
+      this.doorStatus = 'open';
+      this.reportDoorStatus();
+    }
+    this.numberOccupants += numberOfPeople;
+  }
+
+  goToFloor(floor) {
     // update this.floor this.numberTrips
+    
+    this.floor = floor;
+    this.numberTrips++;
+    console.log(`Elevator ${ this.name } is on floor ${ this.floor }. It has taken ${ this.numberTrips } trip${ this.numberTrips > 1 ? 's' : '' }.`);
   }
 }
 
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Building = require('./building');
+const Elevator = require('./elevator');
 
-const twoStoryMansion = new Building(1, 2);
+// const twoStoryMansion = new Building(1, 2);
 
-console.log(twoStoryMansion);
+// console.log(twoStoryMansion);
+
+const myElevator = new Elevator(2);
+myElevator.goToFloor(2);
+myElevator.goToFloor(1);
+myElevator.goToFloor(5);
+myElevator.goToFloor(-1);
