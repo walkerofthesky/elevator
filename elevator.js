60006794093c823faaec52774c480b5b7fa05316
Dustin Parker <dustin.the.park@gmail.com>
2018-05-24 07:35:14 -0600

    Set the door status, add goToFloor conditionals, add exitElevator method
    * Elevator will stay on floor if same is requested
    * Elevator cannot go above maxFloor or below min (1)

--- elevator.js
+++ elevator.js
@@ -8,29 +8,47 @@ class Elevator {
     this.numberTrips = 0;
     this.isInMaintenance = false;
 
-    this.reportDoorStatus = this.reportDoorStatus.bind(this);
+    this.openDoor = this.openDoor.bind(this);
     this.enterElevator = this.enterElevator.bind(this);
     this.goToFloor = this.goToFloor.bind(this);
   }
 
-  reportDoorStatus() {
+  openDoor(open = true) {
+    this.doorStatus = open ? 'open' : 'closed';
     console.log(`Elevator ${ this.name }'s door is ${ this.doorStatus }` );
   }
 
   enterElevator(numberOfPeople) {
     if (this.doorStatus === 'closed') {
-      this.doorStatus = 'open';
-      this.reportDoorStatus();
+      this.openDoor();
     }
     this.numberOccupants += numberOfPeople;
   }
 
   goToFloor(floor) {
     // update this.floor this.numberTrips
+    if (floor === this.floor) {
+      return console.log(`Elevator ${ this.name } is already on floor ${ floor }.`);
+    }
     
+    if (floor > this.maxFloor) {
+      floor = this.maxFloor;
+    } else if (floor < 1) {
+      floor = 1;
+    }
+
+    this.openDoor(false);
+
     this.floor = floor;
     this.numberTrips++;
     console.log(`Elevator ${ this.name } is on floor ${ this.floor }. It has taken ${ this.numberTrips } trip${ this.numberTrips > 1 ? 's' : '' }.`);
+  }
+
+  exitElevator(numberOfPeople) {
+    if (this.doorStatus === 'closed') {
+      this.openDoor();
+    }
+    this.numberOccupants += numberOfPeople;
   }
 }
 
--- index.js
+++ index.js
@@ -8,5 +8,6 @@ const Elevator = require('./elevator');
 const myElevator = new Elevator(2);
 myElevator.goToFloor(2);
 myElevator.goToFloor(1);
+myElevator.goToFloor(1);
 myElevator.goToFloor(5);
 myElevator.goToFloor(-1);
