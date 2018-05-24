ecdd82396eaa3f8eaed07f8daaeda3cf39c25120
Dustin Parker <dustin.the.park@gmail.com>
2018-05-24 08:26:44 -0600

    Add method to get nearest elevator
    * Return all the occupied elevators that are on the move
    * Return all unoccupied elevators
    * Add TODO items to a use the requested elevator to request a floor and have people exit

--- building.js
+++ building.js
@@ -13,6 +13,26 @@ class Building {
     this.requestElevator = this.requestElevator.bind(this);
   }
 
+  getNearestElevator(elevators, currentFloor) {
+    const nearest = {
+      elevator: null,
+      distance: null,
+    };
+    elevators.forEach((elevator, i) => {
+      const elevatorDistance = Math.max(currentFloor, elevator.floor) - Math.min(currentFloor, elevator.floor);
+
+      if (i === 0) {
+        nearest.elevator = elevator;
+        nearest.distance = elevatorDistance;
+      } else {
+        nearest.elevator = elevatorDistance < nearest.distance ? elevator : nearest.elevator;
+        distance = Math.min(elevatorDistance, nearest.distance);
+      }
+    });
+
+    return nearest.elevator;
+  };
+
   requestElevator(currentFloor, numberOfPeople = 1) {
     let availableElevator;
     // an unoccupied elevator is already stopped at that floor
@@ -21,10 +41,17 @@ class Building {
       availableElevator.enterElevator(numberOfPeople);
       return availableElevator;
     }
+
     // an occupied elevator is moving and will pass that floor on its way
-    const occupiedElevators = this.elevators.filter(elevator => elevator.numberOccupants > 0);
+    const occupiedElevators = this.elevators.filter(elevator => elevator.numberOccupants > 0 && elevator.doorStatus === 'closed');
+    // TODO: if we have occupied elevators that are on the move (doors are closed), then return the nearest one
+
     // the unoccupied elevator closest to it will answer the call
+    const unoccupiedElevators = this.elevators.filter(elevator => elevator.numberOccupants === 0);
+    // TODO: return the nearest one
   }
+
+  // TODO: request a floor, and upon arriving exit elevator
 }
 
 module.exports = Building;
